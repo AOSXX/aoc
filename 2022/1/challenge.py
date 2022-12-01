fd095c0097e5731eedfaac6fc972df6d2a368b7d
Jerome Drescig <jeromedrescig@gmail.com>
2022-12-01 07:05:21 +0100

    first exercise

---
+++ 2022/1/challenge.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+def _read_file(filename):
+    """
+    Read lines from a text file called <filename> in the current directory.
+    """
+    with open(filename, "r") as fo:
+        lines = [line.strip() for line in fo.readlines()]
+    return lines
+
+
+
+
+if __name__ == "__main__":
+    with open("data.txt") as f:
+        elves = f.read().split("\n\n")
+        elves = [
+            [int(cal) for cal in elf.split("\n")]
+                           for elf in elves]
+    total_cal = sorted([sum(elf) for elf in elves], reverse=True)
+
+    print(f"First exercise {total_cal[0]} calories.")
+    print(f"Second exercise {sum(total_cal[:3])} calories.")
